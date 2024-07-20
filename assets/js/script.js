bbc45f362f1d0e44dd69f1d1186fdc2ade88fbe3
raccoon254 <tomsteve187@gmail.com>
2024-07-20 15:29:21 +0300

    Complete shuffle name animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,3 +131,33 @@ window.addEventListener('resize', () => {
 	document.querySelector('.background').remove();
 	createBackground();
 });
+
+//ABCDEFGHIJKLMNOPQRSTUVWXYZ
+const letters = "abcdefghijklmnopqrstuvwxyz";
+
+let interval = null;
+
+document.querySelector(".name").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
